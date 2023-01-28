e57f66d6b0fbded5e7134c4304137eba91b9ce4a
jordonss <nemezg@gmail.com>
2023-01-28 21:24:06 +0200

    doma

--- app/js/app.js
+++ app/js/app.js
@@ -7,199 +7,199 @@ import ScrollTrigger from "gsap/ScrollTrigger.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  // var love = setInterval(function () {
-  //   var r_num = Math.floor(Math.random() * 40) + 1;
-  //   var r_size = Math.floor(Math.random() * 65) + 10;
-  //   var r_left = Math.floor(Math.random() * 100) + 1;
-  //   var r_bg = Math.floor(Math.random() * 25) + 100;
-  //   var r_time = Math.floor(Math.random() * 5) + 5;
+	// var love = setInterval(function () {
+	//   var r_num = Math.floor(Math.random() * 40) + 1;
+	//   var r_size = Math.floor(Math.random() * 65) + 10;
+	//   var r_left = Math.floor(Math.random() * 100) + 1;
+	//   var r_bg = Math.floor(Math.random() * 25) + 100;
+	//   var r_time = Math.floor(Math.random() * 5) + 5;
 
-  //   $(".bg_heart").append(
-  //     "<div class='heart' style='width:" +
-  //       r_size +
-  //       "px;height:" +
-  //       r_size +
-  //       "px;left:" +
-  //       r_left +
-  //       "%;background:rgba(255," +
-  //       (r_bg - 25) +
-  //       "," +
-  //       r_bg +
-  //       ",1);-webkit-animation:love " +
-  //       r_time +
-  //       "s ease;-moz-animation:love " +
-  //       r_time +
-  //       "s ease;-ms-animation:love " +
-  //       r_time +
-  //       "s ease;animation:love " +
-  //       r_time +
-  //       "s ease'></div>"
-  //   );
+	//   $(".bg_heart").append(
+	//     "<div class='heart' style='width:" +
+	//       r_size +
+	//       "px;height:" +
+	//       r_size +
+	//       "px;left:" +
+	//       r_left +
+	//       "%;background:rgba(255," +
+	//       (r_bg - 25) +
+	//       "," +
+	//       r_bg +
+	//       ",1);-webkit-animation:love " +
+	//       r_time +
+	//       "s ease;-moz-animation:love " +
+	//       r_time +
+	//       "s ease;-ms-animation:love " +
+	//       r_time +
+	//       "s ease;animation:love " +
+	//       r_time +
+	//       "s ease'></div>"
+	//   );
 
-  //   $(".bg_heart").append(
-  //     "<div class='heart' style='width:" +
-  //       (r_size - 10) +
-  //       "px;height:" +
-  //       (r_size - 10) +
-  //       "px;left:" +
-  //       (r_left + r_num) +
-  //       "%;background:rgba(255," +
-  //       (r_bg - 25) +
-  //       "," +
-  //       (r_bg + 25) +
-  //       ",1);-webkit-animation:love " +
-  //       (r_time + 5) +
-  //       "s ease;-moz-animation:love " +
-  //       (r_time + 5) +
-  //       "s ease;-ms-animation:love " +
-  //       (r_time + 5) +
-  //       "s ease;animation:love " +
-  //       (r_time + 5) +
-  //       "s ease'></div>"
-  //   );
+	//   $(".bg_heart").append(
+	//     "<div class='heart' style='width:" +
+	//       (r_size - 10) +
+	//       "px;height:" +
+	//       (r_size - 10) +
+	//       "px;left:" +
+	//       (r_left + r_num) +
+	//       "%;background:rgba(255," +
+	//       (r_bg - 25) +
+	//       "," +
+	//       (r_bg + 25) +
+	//       ",1);-webkit-animation:love " +
+	//       (r_time + 5) +
+	//       "s ease;-moz-animation:love " +
+	//       (r_time + 5) +
+	//       "s ease;-ms-animation:love " +
+	//       (r_time + 5) +
+	//       "s ease;animation:love " +
+	//       (r_time + 5) +
+	//       "s ease'></div>"
+	//   );
 
-  //   $(".heart").each(function () {
-  //     var top = $(this)
-  //       .css("top")
-  //       .replace(/[^-\d\.]/g, "");
-  //     var width = $(this)
-  //       .css("width")
-  //       .replace(/[^-\d\.]/g, "");
-  //     if (top <= -100 || width >= 150) {
-  //       $(this).detach();
-  //     }
-  //   });
-  // }, 500);
+	//   $(".heart").each(function () {
+	//     var top = $(this)
+	//       .css("top")
+	//       .replace(/[^-\d\.]/g, "");
+	//     var width = $(this)
+	//       .css("width")
+	//       .replace(/[^-\d\.]/g, "");
+	//     if (top <= -100 || width >= 150) {
+	//       $(this).detach();
+	//     }
+	//   });
+	// }, 500);
 
-  AOS.init();
+	AOS.init();
 
-  new Glide(".glide", {
-    type: 'carousel',
-    autoplay: 2000,
-    perView: 3,
-    gap: 30,
-    hoverpause: true,
-    animationDuration: 3100,
-    breakpoints: {
-      767: { perView: 1 },
-      1200: { perView: 3 },
-    }
-  }).mount();
+	new Glide(".glide", {
+		type: 'carousel',
+		autoplay: 2000,
+		perView: 3,
+		gap: 30,
+		hoverpause: true,
+		animationDuration: 3100,
+		breakpoints: {
+			767: { perView: 1 },
+			1200: { perView: 3 },
+		}
+	}).mount();
 
 
-  gsap.registerPlugin(ScrollTrigger);
-  
-  gsap.to("#car", {
-    scrollTrigger: {
-      scrub: true,
-    },
-    y: -900,
-    x: 500,
-  });
+	gsap.registerPlugin(ScrollTrigger);
 
-  let mm = gsap.matchMedia();
+	gsap.to("#car", {
+		scrollTrigger: {
+			scrub: true,
+		},
+		y: -900,
+		x: 500,
+	});
 
-  mm.add("(max-width: 414px)", () => {
+	let mm = gsap.matchMedia();
 
-    gsap.to("#car", {
-      scrollTrigger: {
-        scrub: true,
-      },
-      y: -220,
-      x: 220,
-    });
-  
-  });
+	mm.add("(max-width: 414px)", () => {
 
-  mm.add("(max-width: 575.98px)", () => {
+		gsap.to("#car", {
+			scrollTrigger: {
+				scrub: true,
+			},
+			y: -220,
+			x: 220,
+		});
 
-    gsap.to("#car", {
-      scrollTrigger: {
-        scrub: true,
-      },
-      y: -220,
-      x: 220,
-    });
-  
-  });
+	});
 
-  mm.add("(max-width: 767.98px)", () => {
+	mm.add("(max-width: 575.98px)", () => {
 
-    gsap.to("#car", {
-      scrollTrigger: {
-        scrub: true,
-      },
-      y: -220,
-      x: 220,
-    });
-  
-  });
+		gsap.to("#car", {
+			scrollTrigger: {
+				scrub: true,
+			},
+			y: -220,
+			x: 220,
+		});
 
-  mm.add("(max-width: 992px)", () => {
+	});
 
-    gsap.to("#car", {
-      scrollTrigger: {
-        scrub: true,
-      },
-      y: -220,
-      x: 220,
-    });
-  
-  });
+	mm.add("(max-width: 767.98px)", () => {
 
-  mm.add("(max-width: 1200px)", () => {
+		gsap.to("#car", {
+			scrollTrigger: {
+				scrub: true,
+			},
+			y: -220,
+			x: 220,
+		});
 
-    gsap.to("#car", {
-      scrollTrigger: {
-        scrub: true,
-      },
-      y: -220,
-      x: 220,
-    });
-  
-  });
+	});
 
-  mm.add("(max-width: 1900px)", () => {
+	mm.add("(max-width: 992px)", () => {
 
-    gsap.to("#car", {
-      scrollTrigger: {
-        scrub: true,
-      },
-      y: -220,
-      x: 320,
-    });
-  
-  });
+		gsap.to("#car", {
+			scrollTrigger: {
+				scrub: true,
+			},
+			y: -220,
+			x: 220,
+		});
 
-  window.onload = function () {
-    document.body.classList.add("loaded_hiding");
-    window.setTimeout(function () {
-      document.body.classList.add("loaded");
-      document.body.classList.remove("loaded_hiding");
-    }, 8000);
-  };
+	});
 
-  let welcome = document.getElementById("wrap");
-  let navigation = document.getElementById("navi");
-  let fpContent = document.getElementById("fp-content");
-  let start = document.getElementById("startt");
-  let push = document.getElementById('pbw')
-  // let car = document.getElementById("startCar");
+	mm.add("(max-width: 1200px)", () => {
 
-  welcome.addEventListener("click", function () {
-    welcome.classList.add("welcome-animate");
-    push.classList.add('roll-out-left');
-    setTimeout(function () {
-      navigation.classList.add("navig");
-      fpContent.classList.add("fp-opacity");
-    }, 4000);
-    setTimeout(function () {
-      start.classList.add("start-loaded");
-    }, 4500);
-  });
+		gsap.to("#car", {
+			scrollTrigger: {
+				scrub: true,
+			},
+			y: -220,
+			x: 220,
+		});
 
-  // window.addEventListener('scroll', function () {
-  //   car.style.scale = 120 - +window.pageYOffset/30+'%';
-  // })
-  
+	});
+
+	mm.add("(max-width: 1900px)", () => {
+
+		gsap.to("#car", {
+			scrollTrigger: {
+				scrub: true,
+			},
+			y: -220,
+			x: 320,
+		});
+
+	});
+
+	window.onload = function () {
+		document.body.classList.add("loaded_hiding");
+		window.setTimeout(function () {
+			document.body.classList.add("loaded");
+			document.body.classList.remove("loaded_hiding");
+		}, 8000);
+	};
+
+	let welcome = document.getElementById("wrap");
+	let navigation = document.getElementById("navi");
+	let fpContent = document.getElementById("fp-content");
+	let start = document.getElementById("startt");
+	let push = document.getElementById('pbw')
+	// let car = document.getElementById("startCar");
+
+	welcome.addEventListener("click", function () {
+		welcome.classList.add("welcome-animate");
+		push.classList.add('roll-out-left');
+		setTimeout(function () {
+			navigation.classList.add("navig");
+			fpContent.classList.add("fp-opacity");
+		}, 4000);
+		setTimeout(function () {
+			start.classList.add("start-loaded");
+		}, 4500);
+	});
+
+	// window.addEventListener('scroll', function () {
+	//   car.style.scale = 120 - +window.pageYOffset/30+'%';
+	// })
+
 });
